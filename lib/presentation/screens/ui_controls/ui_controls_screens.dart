9fe0277c8cb44325b1db50dea3f621161e256700
URR505 <mizaelmizrrainub@gmail.com>
2025-01-04 20:24:29 -0600

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/ui_controls/ui_controls_screens.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UiControlsScreens extends StatelessWidget {
+  static const String name = 'ui_control_screen';
+  const UiControlsScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
